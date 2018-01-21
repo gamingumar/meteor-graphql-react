f7d06b9d70731c4a8ca089aa51e832ba5bba4fa9
gamingumar <umaraamer@gmail.com>
2018-01-22 01:38:40 +0500

    graphql schema and basic apollo server setup

--- apollo/imports/startup/server/index.js
+++ apollo/imports/startup/server/index.js
@@ -1,6 +1,26 @@
 import { createApolloServer } from 'meteor/apollo';
 import { makeExecutableSchema } from 'graphql-tools';
 
-createApolloServer({});
+const typeDefs = `
+type Query {
+    hi: String
+}
+`;
+
+const resolvers = {
+  Query: {
+    hi() {
+      return "Hello gamingumar";
+    }
+  }
+}
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+createApolloServer({ schema });
 console.log('hi from server');
-// TODO - START FROM 5
+// TODO - START FROM 6
+// graphql schema and basic apollo server setup
